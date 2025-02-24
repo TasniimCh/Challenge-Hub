e93a4eed3dea72f546ff49c97e3c520f40d41549
nichan said <said.nichan14@gmail.com>
2025-02-24 21:27:38 +0000

    ajouter le project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+     <router-view />
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue' // Adjust path as needed
+
+export default {
+  name: 'App',
+  components: {
+    SignIn
+  }
+}
+</script>
